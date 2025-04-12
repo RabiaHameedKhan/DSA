d8900ffb61de806c75b5918cd77c492ecc94b779
Rabia Khan <154896850+RabiaHameedKhan@users.noreply.github.com>
2025-04-12 06:44:31 +0000

    stack updated

--- stack1.cpp
+++ stack1.cpp
@@ -1,62 +1,83 @@
 #include <iostream>
 using namespace std;
 
-//STACK: LIFO
+// STACK: LIFO
 
+#define SIZE 100 // size of stack
 
-#define SIZE 100; // size of stack
-
-class stack{
-    public:
-
-    int array[SIZE]; //array to store stack
+class Stack {
+public:
+    int array[SIZE]; // array to store stack elements
     int top;
 
-    stack(){// constructor
-        top=-1; //initially empty stack
+    Stack() { // constructor
+        top = -1; // initially empty stack
     }
 
-    // isEmpty operation to check if stak is empty
-    bool isEmpty(){
-        return top=-1;
-    }
-    //isFull operation to check if stack is full
-    bool isFull(){
-        return top=SIZE-1; // size -1 q k index zero se start hota hai
+    // Check if stack is empty
+    bool isEmpty() {
+        return top == -1;
     }
 
-    //Push operation to add elements into stack
-   void push(int value){
-    if(isFull()){
-        cout<<"Stack Overflow! Elements cant be pushed."<<endl;
-        return;
+    // Check if stack is full
+    bool isFull() {
+        return top == SIZE - 1; // because array starts from index 0
     }
-    else{
-        array[++top]=value;
-        cout<<"Value pushed into stack!"<<endl;
+
+    // Push operation to add elements to stack
+    void push(int value) {
+        if (isFull()) {
+            cout << "Stack Overflow! Elements can't be pushed." << endl;
+        } else {
+            array[++top] = value;
+            cout << value << " pushed into stack!" << endl;
+        }
     }
-   }
-   //Pop operation to remove and return the top element
-   void pop(){
-    if(isEmpty()){
-        cout<<"Stack Underflow! Nothing to pop"<<endl;
-        return;
 
+    // Pop operation to remove and return the top element
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack Underflow! Nothing to pop." << endl;
+        } else {
+            cout << array[top--] << " popped from stack." << endl;
+        }
     }
-    else{
-        cout<<array[top--]<<" popped from stack"<<endl;
+
+    // Peek operation to see top element without removing
+    void peek() {
+        if (isEmpty()) {
+            cout << "Stack Underflow! Nothing to peek." << endl;
+        } else {
+            cout << "Top element is: " << array[top] << endl;
+        }
     }
-   }
 
-   //Peek operation to return the top element without removing
+    // Display all elements from top to bottom
+    void display() {
+        if (isEmpty()) {
+            cout << "Stack is empty. Nothing to display." << endl;
+        } else {
+            cout << "Stack elements (top to bottom): ";
+            for (int i = top; i >= 0; i--) {
+                cout << array[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
 
-   void peek(){
+int main() {
+    Stack s;
+    s.push(10);
+    s.push(50);
+    s.push(60);
 
-   }
+    s.display();
 
+    s.pop();
+    s.peek();
 
+    s.display();
 
-
-
-
+    return 0;
 }
